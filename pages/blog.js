33b3eb532eb4ef396378e67ed4cff8d9f94a09e0
Carlos Rodríguez <juminstock@gmail.com>
2023-08-26 18:31:35 -0500

    First pages added

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>
+                ¡Hola, comunidad de Astar Network!
+            </h1>
+        </div>
+    );
+};
+
+export default Blog;
---
+++ pages/equipo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Equipo = () => {
+    return (
+        <div>
+            <h1>
+                ¡Hola, comunidad de Astar Network!
+            </h1>
+        </div>
+    );
+};
+
+export default Equipo;
